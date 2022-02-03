4add4f86d1cf1094c4760b9a8b15b2b54aab7b53
hitalo07 <hitalocunhadesousa@gmail.com>
2022-02-02 23:09:03 -0300

    feat: header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,29 @@
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/form/Input'
+
 export default function Home() {
   return (
-   <h1>Dashboard</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justify="center"
+    >
+      <Flex
+        flexDirection="column"
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+      >
+        <Stack spacing="4">
+          <Input name="email" label="Email" />
+          <Input name="password" label="Senha" />
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
